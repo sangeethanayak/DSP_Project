2b96b1dce97b6e85b0b2be12730cdf671d233059
sangeetha nayak <sangeethanayak333@gmail.com>
2023-03-25 08:56:08 +0530

    plot

--- dataread.py
+++ dataread.py
@@ -1,19 +1,27 @@
-
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
 import csv
+import numpy as np
 with open('datastore.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     rows = list(csv_reader)
-    for j in range(4,120):
+    for j in range(4,119):
           s = rows[j]
           new_list = s[2:770]
-          #print(s)
+                                                                                                                                                                                                                                                                                                                                            
           #print(new_list)
           norm = [float(i)/float(max(new_list)) for i in new_list]
-          print(norm)
+          
+          #print(norm)
+          #plt.plot(float(new_list1),float(norm[2]))
+          #plt.show()
+          
     j=j+1
+plt.plot(norm)
+plt.ylabel('amplitude')
+plt.show()
     
-#norm = [float(i)/float(max(new_list)) for i in new_list]
-#print(norm)
+
         
     
 '''
