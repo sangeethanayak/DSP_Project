abb341d367d9887477201be3268575a31757de10
Sangeetha Nayak <sangeethanayak333@gmail.com>
2023-05-08 22:18:36 +0530

    added Energy and Entropy

--- dataread.py
+++ dataread.py
@@ -5,7 +5,7 @@ import numpy as np
 from collections import OrderedDict
 from scipy.signal import butter, filtfilt, welch
 from scipy import signal
-from scipy.signal import firwin, lfilter, welch
+from scipy.signal import firwin, lfilter, welch, freqz
 from scipy.signal.windows import hamming
 from numpy import convolve
 
@@ -28,10 +28,10 @@ with open('datastore.csv') as csv_file:
 
 #print(floats)
 
-x_values=np.arange(len(floats))*0.03125
+x_values=np.arange(len(floats))*0.0625
 plt.plot(x_values,floats)
 #plt.xticks(np.arange(0,0.03125), fontsize=8)
-plt.xticks(np.arange(0, len(floats)*0.03125, 1), fontsize=8)
+plt.xticks(np.arange(0, len(floats)*0.0625, 2), fontsize=8)
 #print(len(floats)*0.03125)
 plt.xlabel('Time (s)')
 plt.ylabel('Normalized Values')
@@ -39,29 +39,22 @@ plt.show()
 
 
 
-fs = 32  # sampling frequency
-fc = 0.5  # cutoff frequency
-N = 101  # number of taps
-b = signal.firwin(N, fc, pass_zero=False, window='hamming', fs=fs)
-filtered_data= signal.convolve(floats, b, mode='same')
+# Apply bandpass filter
+fs = 16  # Sampling rate (Hz)
+f_low = 0.1  # Lower cutoff frequency (Hz)
+f_high = 0.5  # Upper cutoff frequency (Hz)
+order = 4  # Filter order
+b, a = signal.butter(order, [f_low * 2 / fs, f_high * 2 / fs], btype='band')
+filtered_data = signal.filtfilt(b, a, floats)
 
-# plot filtered signal
-plt.plot(filtered_data)
-plt.xlabel('Sample')
-plt.ylabel('Amplitude')
+# Plot filtered signal
+x_values = np.arange(len(filtered_data)) / fs
+plt.plot(x_values, filtered_data)
+plt.xlabel('Time (s)')
+plt.ylabel('Filtered Values')
 plt.show()
 
 
-"""
-#filter specifications
-fs = 153.3
-fs1 = 32
-cutoff = 10
-nyq = 0.5 * fs
-order = 4 
-b, a = butter(order, cutoff/nyq, btype='low')
-filtered_data = filtfilt(b, a, floats)
-"""
 
 #fft specifications
 
@@ -96,9 +89,24 @@ plt.show()
 
 
 max_index = np.argmax(psd1)
-breathing_rate = xf1[max_index]
-print("Breathing rate: {:.2f} Hz".format(breathing_rate))
+breathing = xf1[max_index]
+breathing_rate= breathing*60
+print("Breathing rate: {:.2f} bpm".format(breathing_rate))
+
+# Calculate time domain energy and entropy for filtered_data
+energy_time = np.sum(np.square(filtered_data))
+hist, bins = np.histogram(filtered_data, bins='auto', density=True)
+prob = hist / np.sum(hist)
+entropy_time = -np.sum(prob * np.log2(prob))
+print('Energy in time domain=',energy_time)
+print('Entropy in time domain=',entropy_time)
 
 
-        
 
+# Calculate frequency domain energy and entropy for filtered_data
+freq, psd = welch(filtered_data, fs=fs, nperseg=256)
+energy_freq = np.sum(psd)
+prob_freq = psd / np.sum(psd)
+entropy_freq = -np.sum(prob_freq * np.log2(prob_freq))
+print('Energy in freq domain=',energy_freq)
+print('Entropy in freq domain=',entropy_freq)
